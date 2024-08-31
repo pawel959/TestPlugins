d2f815e597def7fcfc2415ed2e740d0b297c9d27
pawel959 <pawprz19@o2.pl>
2024-08-31 02:14:24 +0200

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -10,7 +10,7 @@ version = -1
 cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    description = "Lorem ipsum"
+    description = "Test"
     authors = listOf("Cloudburst")
 
     /**
